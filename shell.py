f6a6606adaa8e94099a20e1cb5827e9718692726
sodkun <shodikkunachmat21@students.unnes.ac.id>
2022-05-26 11:17:18 +0700

    final project

--- shell.py
+++ shell.py
@@ -1,8 +1,14 @@
-import Basic
+import basic
 
 while True:
-		text = input('javanglish > ')
-		result, error = Basic.run('<stdin>', text)
+	text = input('javanglish > ')
+	if text.strip() == "": continue
+	result, error = basic.run('<stdin>', text)
 
-		if error: print(error.as_string())
-		else: print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
